815686868498d41100ef95c7e69c48a0f34246e6
Patrick Gardella <patrick.gardella@asburyseminary.edu>
2016-12-15 20:26:54 -0500

    Add Berks Make foodcritic compliant Make cookstyle compliant Test using apt-get remove

--- cookbooks/raspberry/metadata.rb
+++ cookbooks/raspberry/metadata.rb
@@ -5,3 +5,5 @@ license          'AGPL3'
 description      'Sets up a Raspberry Pi to use Chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+issues_url 'https://github.com/patrickgardella/ppi_chef/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/patrickgardella/ppi_chef/' if respond_to?(:source_url)
--- cookbooks/raspberry/recipes/default.rb
+++ cookbooks/raspberry/recipes/default.rb
@@ -1,50 +1,58 @@
-cookbook_file "/etc/network/interfaces" do
-  source "interfaces"
+cookbook_file '/etc/network/interfaces' do
+  source 'interfaces'
 end
 
-apt_package 'iw'
-apt_package 'wpasupplicant'
-
-cookbook_file "/boot/wifi.conf" do
-	action :create_if_missing
-	source "wifi.conf"
+apt_update 'Update the apt cache' do
+  action :update
 end
 
-apt_package 'git'
-apt_package 'python-git'
-apt_package 'logrotate'
+package 'iw'
+package 'wpasupplicant'
 
-cookbook_file "/etc/logrotate.d/update_chef" do
-    source "update_chef_logrotate"
+cookbook_file '/boot/wifi.conf' do
+  action :create_if_missing
+  source 'wifi.conf'
 end
 
-cookbook_file "/usr/local/bin/update_chef.py" do
-    source "update_chef.py"
-    mode '0755'
+package 'git'
+package 'python-git'
+package 'logrotate'
+
+package 'sonic-pi' do
+  action :remove
 end
 
-file "/boot/chef-url" do
-    action :create_if_missing
-    content "http://foo.bar"
+cookbook_file '/etc/logrotate.d/update_chef' do
+  source 'update_chef_logrotate'
 end
 
-file "/boot/chef-cookbook" do
-    action :create_if_missing
-    content ""
+cookbook_file '/usr/local/bin/update_chef.py' do
+  source 'update_chef.py'
+  mode '0755'
 end
 
-cookbook_file "/etc/init.d/update_chef" do
-    source "update_chef_init"
-    mode '0755'
+file '/boot/chef-url' do
+  action :create_if_missing
+  content 'http://foo.bar'
+end
+
+file '/boot/chef-cookbook' do
+  action :create_if_missing
+  content ''
+end
+
+cookbook_file '/etc/init.d/update_chef' do
+  source 'update_chef_init'
+  mode '0755'
 end
 
 apt_package 'ruby-dev'
 gem_package 'berkshelf' do
-    version '4.3.3'
+  version '4.3.3'
 end
 
-service "update_chef" do
-    supports :status => true, :restart => true, :reload => true
-    action [ :enable, :start ]
-    provider Chef::Provider::Service::Init::Debian
+service 'update_chef' do
+  supports status: true, restart: true, reload: true
+  action [:enable, :start]
+  provider Chef::Provider::Service::Init::Debian
 end
